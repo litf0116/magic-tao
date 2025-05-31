43077db787c56ba18d43d9e404bf8e712f5b8660
LiTengFei0312 <799849240@qq.com>
2025-06-01 03:04:18 +0800

    update

---
+++ molitao_uniapp/src/components/chat/AuctionEndMessage.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="border-amber border-2 border-solid py-2 px-4 rounded-lg relative">
+        <div class="absolute top-0 right-0 bg-amber text-white rounded-lb-lg px-2 font-bold text-xs">成功竞拍</div>
+        <div class="max-w-350px min-w-200px" @tap="handleShowDetails">
+            <!-- 已成交状态 -->
+            <div v-if="payloadData.status === '已成交'">
+                <div class="text-red-500">
+                    恭喜 {{ payloadData.dealUserName }} 最终以
+                    <b class="text-lg">￥{{ payloadData.finalPrice }}</b> 拍得商品
+                </div>
+                <div class="text-sm">商品名称: {{ payloadData.name }}</div>
+                <div class="text-sm">{{ formattedDealTime }}</div>
+                <div class="mt-2 text-sm text-gray-600">
+                    买卖双方私聊拍卖师确认交易!<br />
+                    认准星标小心冒充<br />
+                    有请下一件拍品
+                </div>
+            </div>
+            <!-- 流拍状态 -->
+            <div v-else-if="payloadData.status === '上架'">
+                <div>商品流拍</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { computed } from 'vue'
+import { type ChatMessage } from '@/composables/types'
+import dayjs from 'dayjs'
+
+export default {
+    name: 'AuctionEndMessage',
+    props: {
+        message: {
+            type: Object as () => ChatMessage,
+            required: true,
+        },
+    },
+    emits: ['showDetails'],
+    setup(props: any, { emit }: any) {
+        // 解析payload数据
+        const payloadData = computed(() => {
+            let payload = props.message.payload
+            if (typeof payload === 'string') {
+                payload = JSON.parse(payload!)
+            }
+            return payload
+        })
+
+        // 格式化交易时间
+        const formattedDealTime = computed(() => {
+            if (payloadData.value.dealTime) {
+                return dayjs(payloadData.value.dealTime).format('YYYY-MM-DD HH:mm:ss')
+            }
+            return ''
+        })
+
+        // 处理点击显示详情事件
+        const handleShowDetails = () => {
+            emit('showDetails', props.message)
+        }
+
+        return {
+            payloadData,
+            formattedDealTime,
+            handleShowDetails,
+        }
+    },
+}
+</script>
--- molitao_uniapp/src/components/chat/auctionMidList.vue
+++ molitao_uniapp/src/components/chat/auctionMidList.vue
@@ -1,19 +1,13 @@
 <template>
     <view class="shadow flex flex-row items-center overflow-hidden cursor-pointer" @click.stop="showDetail(item)">
-        <div class="text-[#935F4E] line-clamp-3" style="margin-right: 5px;">{{ index }}.</div>
+        <div class="text-[#935F4E] line-clamp-3" style="margin-right: 5px">{{ index }}.</div>
         <image :src="getImgUrl(item.imageUrl!, true)" class="w-16 h-16 rounded" mode="aspectFill" />
         <view class="text-wrap text-xs px-2 flex-1 flex flex-col">
             <view class="text-[#935F4E] line-clamp-3">{{ item.name }}</view>
-            <div v-if="item.currentPrice" class="flex" style="flex-wrap: wrap;">
-                <div class="text-red-500" style="width: 150px;">
-                    出价人:{{ item.currentPriceUserName }}
-                </div>
-                <div class="text-red-500">
-                    当前出价:￥{{ item.currentPrice }}
-                </div>
-                <div class="text-red-500 " style="margin-bottom:5px;">
-                    倒计时: {{ minutes }}分钟{{ seconds }}秒
-                </div>
+            <div v-if="item.currentPrice" class="flex" style="flex-wrap: wrap">
+                <div class="text-red-500" style="width: 150px">出价人:{{ item.currentPriceUserName }}</div>
+                <div class="text-red-500">当前出价:￥{{ item.currentPrice }}</div>
+                <div class="text-red-500" style="margin-bottom: 5px">倒计时: {{ minutes }}分钟{{ seconds }}秒</div>
             </div>
         </view>
     </view>
@@ -22,7 +16,7 @@
 <script setup lang="ts">
 import type { AuctionItemDto } from '@/composables/types'
 import { getImgUrl } from '@/composables/index'
-import { useCountdown } from '@/utils/countdown';
+import { useCountdown } from '@/utils/countdown'
 
 const props: any = defineProps({
     item: {
@@ -30,8 +24,8 @@ const props: any = defineProps({
         required: true,
     },
     index: {
-        required: true
-    }
+        required: true,
+    },
 })
 const emit = defineEmits(['showDetail'])
 function showDetail(item: AuctionItemDto) {
@@ -39,7 +33,7 @@ function showDetail(item: AuctionItemDto) {
 }
 onMounted(() => {
     // 开始初始倒计时
-    startCountdown();
+    startCountdown()
 })
 
 // 在组件卸载时停止计时器（如果使用 Vue 的话）
@@ -47,20 +41,16 @@ onBeforeUnmount(() => {
     stopCountdown()
 })
 //检查值是否被修改
-watch(() => props.item, (newValue: any, oldValue) => {
-    resetCountdown(newValue.useCountdownTime)
-})
+watch(
+    () => props.item,
+    (newValue: any, oldValue) => {
+        resetCountdown(newValue.useCountdownTime)
+    }
+)
 // 变量来存储倒计时数据
-const {
-    days,
-    hours,
-    minutes,
-    seconds,
-    isFinished,
-    startCountdown,
-    stopCountdown,
-    resetCountdown
-} = useCountdown(props.item.useCountdownTime)
+const { days, hours, minutes, seconds, isFinished, startCountdown, stopCountdown, resetCountdown } = useCountdown(
+    props.item.useCountdownTime
+)
 // watch(() => seconds.value, (newValue, oldValue) => {
 
 //     debugger
@@ -75,3 +65,11 @@ const checkIncludes = computed(() => {
     font-weight: 600;
 }
 </style>
+
+function defineProps(arg0: { item: { type: PropType<AuctionItemDto>; required: boolean }; index: { required: boolean } }): any {
+  throw new Error('Function not implemented.')
+}
+
+function defineEmits(arg0: string[]) {
+  throw new Error('Function not implemented.')
+}
