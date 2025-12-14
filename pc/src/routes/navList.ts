0d10df810367983a16a947b4af68d44ed7edd6dd
LiTengFei0312 <799849240@qq.com>
2025-12-15 00:40:44 +0800

    fix: 修复菜单项路径冲突问题

--- pc/src/routes/navList.ts
+++ pc/src/routes/navList.ts
@@ -5,5 +5,5 @@ export const navList = [
     { name: '魔力淘橱窗', path: '/a' },
     { name: '魔力宝贝官网', path: '', url: 'https://www.polchina.com.cn' },
     { name: '魔力百科资料站', path: '', url: 'https://www.molibaike.com' },
-    { name: '自助中介系统', path: '/a' },
+    { name: '自助中介系统', path: '/e' },
 ]
