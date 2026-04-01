6901a07841e6874b2bfccaa6fcf34263d737f8e7
LiTengFei0312 <799849240@qq.com>
2026-04-01 12:38:49 +0800

    chore: 修改Android包名为 com.molitao.app
    - namespace: com.molitao.molitao_app → com.molitao.app
    - applicationId: com.molitao.molitao_app → com.molitao.app
    - 移动 MainActivity.kt 到新包路径

--- molitao_app/android/app/build.gradle.kts
+++ molitao_app/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.molitao.molitao_app"
+    namespace = "com.molitao.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.molitao.molitao_app"
+        applicationId = "com.molitao.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
