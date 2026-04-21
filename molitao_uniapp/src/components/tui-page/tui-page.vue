4033404da67c99ec0bbf47274ff805f56e5fee77
LiTengFei0312 <799849240@qq.com>
2026-04-21 20:55:06 +0800

    revert(uniapp): 恢复 tui-page 组件到 4月16日版本
    - 恢复使用 CSS background 方式
    - 移除 tui-page 的 easycom 规则（依赖 autoscan）

--- molitao_uniapp/src/components/tui-page/tui-page.vue
+++ molitao_uniapp/src/components/tui-page/tui-page.vue
@@ -1,25 +1,14 @@
 <template>
-    <view class="min-h-screen bg-true-gray-100 relative dark:bg-true-gray-900 dark:text-white">
-        <!-- 背景图片层 - 使用 image 标签替代 CSS background -->
-        <image
-            v-if="true"
-            class="absolute top-0 left-0 w-full"
-            style="height: 373rpx; z-index: 0"
-            src="https://image.molitao.top/20250330/097jzbhb3jq364wcrsyjw61qb0bj9xob.png"
-            mode="aspectFill"
-        />
-        <view
-            class="relative"
-            style="z-index: 1"
-            :class="[cssClass ? cssClass : '']"
-        >
-            <view v-if="showLoading" class="loading">
-                <!-- <tui-loading></tui-loading> -->
-            </view>
-            <template v-else>
-                <slot />
-            </template>
+    <view
+        class="min-h-screen bg-true-gray-100 relative dark:bg-true-gray-900 dark:text-white"
+        :class="[topBg ? 'topBg' : '', cssClass ? cssClass : '']"
+    >
+        <view v-if="showLoading" class="loading">
+            <!-- <tui-loading></tui-loading> -->
         </view>
+        <template v-else>
+            <slot />
+        </template>
         <!-- <tui-footer /> -->
     </view>
 </template>
