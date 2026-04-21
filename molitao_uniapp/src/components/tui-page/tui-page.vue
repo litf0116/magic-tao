b61a307d2c7fcbb302bab6a0760731e03659b12c
LiTengFei0312 <799849240@qq.com>
2026-04-21 20:38:24 +0800

    fix(uniapp): 使用压缩版背景图片解决小程序不显示问题

--- molitao_uniapp/src/components/tui-page/tui-page.vue
+++ molitao_uniapp/src/components/tui-page/tui-page.vue
@@ -1,14 +1,25 @@
 <template>
-    <view
-        class="min-h-screen bg-true-gray-100 relative dark:bg-true-gray-900 dark:text-white"
-        :class="[topBg ? 'topBg' : '', cssClass ? cssClass : '']"
-    >
-        <view v-if="showLoading" class="loading">
-            <!-- <tui-loading></tui-loading> -->
+    <view class="min-h-screen bg-true-gray-100 relative dark:bg-true-gray-900 dark:text-white">
+        <!-- 背景图片层 - 使用 image 标签替代 CSS background -->
+        <image
+            v-if="topBg"
+            class="absolute top-0 left-0 w-full"
+            style="height: 373rpx; z-index: 0"
+            src="https://image.molitao.top/20250330/097jzbhb3jq364wcrsyjw61qb0bj9xob.png!w300"
+            mode="aspectFill"
+        />
+        <view
+            class="relative"
+            style="z-index: 1"
+            :class="[cssClass ? cssClass : '']"
+        >
+            <view v-if="showLoading" class="loading">
+                <!-- <tui-loading></tui-loading> -->
+            </view>
+            <template v-else>
+                <slot />
+            </template>
         </view>
-        <template v-else>
-            <slot />
-        </template>
         <!-- <tui-footer /> -->
     </view>
 </template>
